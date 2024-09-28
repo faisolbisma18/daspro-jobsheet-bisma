a1c442519bde0217adac9352ab5db86311e596ec
faisolbisma18 <144082490+faisolbisma18@users.noreply.github.com>
2024-09-28 23:46:47 +0700

    Add files via upload

--- PemilihanBilangan10.java
+++ PemilihanBilangan10.java
@@ -8,14 +8,8 @@ public class PemilihanBilangan10{
         System.out.print("masukkan sebuah angka: ");
         int angka = sc.nextInt();
 
-        if (angka % 2 == 0)
-        {
-            System.out.println("angka " + angka + "termasuk bilangan genap");
-        }
-        else
-        {
-            System.out.println("angka " + angka + "termasuk bilangan ganjil");
-        }
+        String hasil = (angka % 2 == 0) ? "termasuk bilangan genap" : "termasuk bilangan ganjil";
+        System.out.println("angka " + angka + " " + hasil);
         
 
     }
