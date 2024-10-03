2e8e36d547a0bea87b18e2dc41eb22b86cbd0b01
faisolbisma18 <144082490+faisolbisma18@users.noreply.github.com>
2024-10-03 12:45:34 +0700

    Add files via upload

---
+++ pemilihan2percobaan1.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class pemilihan2percobaan1 {
+    public static void main(String[] args) {
+    
+        Scanner input10 = new Scanner(System.in);
+
+        System.out.print("Masukkan tahun: ");
+        int tahun = input10.nextInt(); 
+
+        System.out.println("\nInformasi yang dimasukkan:");
+        System.out.println("Tahun: " + tahun);
+
+        if ((tahun % 4) == 0) {
+            if ((tahun % 100) != 0)
+            System.out.println("tahun kabisat");
+        } else
+            System.out.println("bukan tahun kabisat");
+
+        
+    }
+}
