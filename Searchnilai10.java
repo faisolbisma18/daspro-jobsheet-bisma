08a680e8534aaa8aca7304e285e2df3fcab38422
faisolbisma18 <144082490+faisolbisma18@users.noreply.github.com>
2024-11-06 19:35:45 +0700

    Add files via upload

--- Searchnilai10.java
+++ Searchnilai10.java
@@ -3,30 +3,39 @@ public class Searchnilai10 {
     public static void main(String[] args) {
        Scanner sc = new Scanner(System.in); 
     
-    System.out.print("Masukkan banyak nilai yang akan di input: ");
-    int jmlnilai = sc.nextInt();
-    int[] arrnilai =new int[jmlnilai];
+    System.out.print("masukkan jumlah nilai mahasiswa yang akan di input: ");
+    int jumlahnilai = sc.nextInt();
 
-    for (int i = 0; i < jmlnilai; i++) {
-        System.out.print("Masukkan nilai mahasiswa ke-"+(i+1)+" : ");
-        arrnilai[i]=sc.nextInt();
+    int[] nilaiMahasiswa = new int[jumlahnilai];
+
+    for (int i = 0; i < jumlahnilai; i++) {
+        System.out.print("Masukkan nilai mahasiswa ke-" + (i + 1) + ": ");
+        nilaiMahasiswa[i] = sc.nextInt();
     }
-    System.out.print("Masukkan nilai yang akan dicari: ");
-    int key = sc.nextInt();
 
-    int hasil = 0;
-    for(int i=0; i<arrnilai.length; i++) {
-        if (key==arrnilai[i]) {
-            hasil = i+1;
-            break;
+    double totalNilai = 0;
+    for (int nilai : nilaiMahasiswa) {
+        totalNilai += nilai;
+    }
+
+    double ratarata = totalNilai / jumlahnilai;
+
+    int nilaitertinggi = nilaiMahasiswa[0];
+    int nilaiterendah = nilaiMahasiswa[0];
+    for (int nilai : nilaiMahasiswa) {
+        if (nilai > nilaitertinggi) {
+            nilaitertinggi = nilai;
         }
     }
-    System.out.println();
-    if (hasil != 0) {
-        System.out.println("Nilai "+key+" ketemu, merupakan nilai mahasiswa ke-"+hasil);
-    }else{
-        System.out.println("Nilai "+key+"tidak ditemukan dalam array.");
+
+    System.out.println("\nNilai mahasiswa yang telah di masukkan:");
+    for (int nilai : nilaiMahasiswa) {
+        System.out.println(nilai);
     }
+    System.out.println("\nNilai rata-rata: " + ratarata);
+    System.out.println("nilai tertinggi: " + nilaitertinggi);
+    System.out.println("nilai terendah: " + nilaiterendah);
+
     sc.close();
     
 }
