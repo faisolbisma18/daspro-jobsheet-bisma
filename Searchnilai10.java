7106f5b39177e58b4b10aa71e7d1b94e2e5fd3e5
faisolbisma18 <144082490+faisolbisma18@users.noreply.github.com>
2024-11-06 19:11:00 +0700

    Add files via upload

--- Searchnilai10.java
+++ Searchnilai10.java
@@ -1,21 +1,33 @@
 import java.util.Scanner;
 public class Searchnilai10 {
     public static void main(String[] args) {
-        
+       Scanner sc = new Scanner(System.in); 
     
-    int[] arrnilai = {80, 85, 78, 96, 90, 82, 86};
-    int key = 90;
-    int hasil = 0;
+    System.out.print("Masukkan banyak nilai yang akan di input: ");
+    int jmlnilai = sc.nextInt();
+    int[] arrnilai =new int[jmlnilai];
 
-    for (int i = 0; i < arrnilai.length; i++) {
-        if (key == arrnilai[i]) {
-            hasil = i;
+    for (int i = 0; i < jmlnilai; i++) {
+        System.out.print("Masukkan nilai mahasiswa ke-"+(i+1)+" : ");
+        arrnilai[i]=sc.nextInt();
+    }
+    System.out.print("Masukkan nilai yang akan dicari: ");
+    int key = sc.nextInt();
+
+    int hasil = 0;
+    for(int i=0; i<arrnilai.length; i++) {
+        if (key==arrnilai[i]) {
+            hasil = i+1;
             break;
         }
     }
     System.out.println();
-    System.out.println("Nilai "+key+" ketemu di indeks ke-"+hasil);
-    System.out.println();
+    if (hasil != 0) {
+        System.out.println("Nilai "+key+" ketemu, merupakan nilai mahasiswa ke-"+hasil);
+    }else{
+        System.out.println("Nilai "+key+"tidak ditemukan dalam array.");
+    }
+    sc.close();
     
 }
 
